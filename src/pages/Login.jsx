3a8868fc7c30b9529ef0b67f27eb000cf0ed467b
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-01-13 18:15:06 -0300

    feat: Requisito 1s

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,69 @@
 import React from 'react';
+import { createUser } from '../services/userAPI';
+import { Redirect } from 'react-router-dom';
+import Loading from './Loading';
 
+const MINIMUM_CHARACTERS = 3;
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recName: '',
+      savingUser: '',
+      reponseCome: false,
+    };
+  }
+
+  handleSendUser = async (event) => {
+    event.preventDefault();
+    const { recName } = this.state;
+    this.setState({ savingUser: true });
+    const response = await createUser({ name: recName });
+    if (response) return this.setState({ responseCome: true });
+    return this.setState({ savingUser: false, })
+  }
+
+  handleValidate = () => {
+    const { recName } = this.state;
+    if (recName.length >= MINIMUM_CHARACTERS) return true;
+    return false;
+  }
+
+  handleInputChange = ({ target }) => {
+    const { id, value } = target;
+    this.setState({ [id]: value });
+    this.handleValidate;
+  }
+
   render() {
-    return <div data-testid="page-login" />;
+    const { recName, savingUser, responseCome } = this.state;
+    return (
+      <section>
+        { responseCome ? <Redirect to="/search" /> : null }
+        { savingUser ? <Loading /> : null }
+        <div data-testid="page-login">
+          <form action="">
+            <label htmlFor="input-name">
+              Digite seu nome
+              <input
+                type="text"
+                id="recName"
+                data-testid="login-name-input"
+                value={ recName }
+                onChange={ this.handleInputChange }
+              />
+            </label>
+            <button
+              type="submit"
+              data-testid="login-submit-button"
+              disabled={ !this.handleValidate() }
+              onClick={ this.handleSendUser }>
+                Entrar
+            </button>
+          </form>
+        </div>
+      </section>
+    );
   }
 }
 

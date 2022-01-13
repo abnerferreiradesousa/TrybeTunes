e37498380ce3bffa32fef28eb6f3f77ec9849349
Abner Ferreira de Sousa <abnerferreira84@gmail.com>
2022-01-13 18:44:00 -0300

    feat: Requisito 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 class Header extends React.Component {
-  render() { 
+  render() {
     return (
-      <div data-testid="header-component"></div>
+      <div data-testid="header-component" />
     );
   }
 }
- 
+
 export default Header;
